e7e1f2fd7cbab745968de431a3e0252a6ac5ca51
Andres rodriguez Toca <48394901+AndresRodriguezToca@users.noreply.github.com>
2019-08-19 02:31:07 -0400

    Add files via upload

---
+++ src/javaguiprogram/JavaGUIProgram.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaguiprogram;
+
+/**
+ *
+ * @author rodri
+ */
+public class JavaGUIProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frameStaff mainFrame = new frameStaff();
+        mainFrame.setVisible(true);
+    }
+    
+}
